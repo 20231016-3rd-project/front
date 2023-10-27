36a0aef53d7c7f2fe34d58f32d77b96516776a0e
Jun <yuncheol.kim42@gmail.com>
2023-10-27 09:52:39 +0900

    design: 리뷰 별점 레이아웃 작업

---
+++ src/pages/restaurantInfo/Star.tsx
@@ -0,0 +1,37 @@
+import { FaStar } from 'react-icons/fa';
+import { useState } from 'react';
+import styled from 'styled-components';
+
+interface StarProps {
+  score: number;
+}
+
+const Star: React.FC<StarProps> = ({ score }) => {
+  const maxScore = 5;
+  const restScore = maxScore - score;
+
+  const scoreArray = Array.from({ length: score }, () => true);
+  const emptyScoreArray = Array.from({ length: restScore }, () => false);
+  const starArray = [...scoreArray, ...emptyScoreArray];
+
+  //star setting
+  const starSize = 30;
+  const starColor = { filled: '#ffc107', empty: '#e4e5e9' };
+  return (
+    <StarStyle>
+      {starArray.map((isScore, index) => {
+        return (
+          <FaStar
+            key={index}
+            className="rating__star"
+            size={starSize}
+            color={isScore ? starColor.filled : starColor.empty}
+          />
+        );
+      })}
+    </StarStyle>
+  );
+};
+
+export default Star;
+const StarStyle = styled.div``;
