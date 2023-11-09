1b49a21cb4cd01dcf6f49e18893b380c45ee8ae4
suj2n <suj2n.k@gmail.com>
2023-11-09 21:25:19 +0900

    로그인변경사항

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,7 @@ import { RouterProvider } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import {store} from './app/store'; 
 
-import { Provider } from 'react-redux';
-import store from './store/store';
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 
--- src/pages/signIn/SignIn.tsx
+++ src/pages/signIn/SignIn.tsx
@@ -3,38 +3,17 @@ import { useNavigate } from 'react-router-dom';
 import * as S from './SignIn.styles';
 import { KAKAO_AUTH_URI } from '../../apis/kakaoAuthApi/kakaoAuthApi';
 import { handleKakaoLogin } from '../../apis/kakaoAuthApi/kakaoAuthApi';
-import { login } from '../../apis/authApi/authApi'; // 통합된 API 호출
+import { login } from '../../apis/authApi/authApi'; 
 import kakaoLogo from '../../assets/images/kakaoLogo.png';
 
 const SignIn = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
-  
+
   const handleLogin = async (email: string, password: string) => {
     try {
-<<<<<<< HEAD
-      const response = await axios
-        .post(
-          `${import.meta.env.VITE_APP_SERVER_API}sunflowerPlate/user/login`,
-          {
-            email,
-            password,
-          }
-        )
-        .then((r) => {
-          console.log(r);
-          const { accessToken } = r.data;
-          localStorage.setItem('accessToken', accessToken);
-        });
-
-      // Here you would usually store the tokens in some kind of storage (like localStorage)
-      // localStorage.setItem('accessToken', AccessToken);
-      // localStorage.setItem('refreshToken', refreshToken);
-
-=======
       await login({ email, password });
->>>>>>> 5e4434a (feat : 로그인페이지 통합, 카카오소셜로그인 구현)
       navigate('/'); // Redirect to home page after successful login
     } catch (err) {
       // Improved error handling
@@ -50,7 +29,7 @@ const SignIn = () => {
     if (code) {
       try {
         await handleKakaoLogin(code);
-        navigate('/'); // 홈으로 리디렉션
+        navigate('/'); 
       } catch (err) {
         console.error('Error during Kakao login', err);
       }
@@ -77,7 +56,7 @@ const SignIn = () => {
       />
       <S.Button onClick={() => handleLogin(email, password)}>Login</S.Button>
       
-      {/* 카카오 로그인 버튼 */}
+      
       <a href={KAKAO_AUTH_URI}>
         <img src={kakaoLogo} alt="카카오 로그인" />
       </a>
