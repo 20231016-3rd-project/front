2799d1cac925419ee6984cf6a39ab0dcea4c6324
suj2n <suj2n.k@gmail.com>
2023-12-18 17:04:55 +0900

    현재사항반영

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,6 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { setToken } from './store/slices/authSlice';
 import router from '../src/router/Router';
-import axios  from 'axios';
 
 function App() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    const accessToken = localStorage.getItem('accessToken');
-    if (accessToken) {
-      dispatch(setToken({ accessToken }));
-      axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`; // axios 사용하는 경우
-    }
-  }, [dispatch]);
-
   return router;
 }
 
